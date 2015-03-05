bfdbc621d3fb4d4f89c7c8b65b1b3adfb6ec1cac
mrolcsi <m.rolcsi@hotmail.com>
2015-03-05 18:17:20 +0100

    Better layout.

---
+++ LyricsPlayer/script/player.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Roland on 2015.03.05..
+ */
+
+window.onload = function () {
+    //noinspection JSSuspiciousNameCombination
+    var $coverArt = $('#coverArt');
+    var cw = $coverArt.width();
+    $coverArt.css({'height': cw + 'px'});
+};
+
+function openFile() {
+    //TODO
+}
+
+function playPause() {
+    //TODO
+}
+
+function seek() {
+    //TODO
+}
