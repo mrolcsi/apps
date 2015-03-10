6b563b2eb85517998130fd14bc7656f8b6317e85
mrolcsi <m.rolcsi@hotmail.com>
2015-03-11 00:30:48 +0100

    Replace embedded mp3s with remote files.

--- LyricsPlayer/script/player.js
+++ LyricsPlayer/script/player.js
@@ -31,21 +31,23 @@ function loadPompeii() {
     $("#title").text("Pompeii");
     $("#artistAlbum").text("Bastille - Pompeii");
 
-    $("#currentSong").attr("src", "/music/bastille-pompeii.mp3");
+    //$("#currentSong").attr("src", "/music/bastille-pompeii.mp3");
+    $("#currentSong").attr("src", "http://users.atw.hu/mrolcsi/testmp3/bastille-pompeii.mp3");
 }
 function loadVivaLaVida() {
     $("#coverArt").css("background-image", "url('/img/covers/vivalavida.jpg')");
     $("#title").text("Viva la Vida");
     $("#artistAlbum").text("Coldplay - Viva la Vida");
 
-    $("#currentSong").attr("src", "/music/coldplay-vivalavida.mp3");
+    //TODO: use external audio
+    $("#currentSong").attr("src", "http://users.atw.hu/mrolcsi/testmp3/coldplay-vivalavida.mp3");
 }
 function loadClarity() {
     $("#coverArt").css("background-image", "url('/img/covers/clarity.jpg')");
     $("#title").text("Clarity (feat. Foxes)");
     $("#artistAlbum").text("Zedd - Clarity (Deluxe Edition)");
 
-    $("#currentSong").attr("src", "/music/zedd-clarity.mp3");
+    $("#currentSong").attr("src", "http://users.atw.hu/mrolcsi/testmp3/zedd-clarity.mp3");
 }
 
 function playPause() {
@@ -55,7 +57,11 @@ function playPause() {
 
     if (audio.paused) {
         audio.play();
-    } else audio.pause();
+        $("#playPause").attr("src", "/img/player_pause.png");
+    } else {
+        audio.pause();
+        $("#playPause").attr("src", "/img/player_play.png");
+    }
 }
 
 function seek() {
